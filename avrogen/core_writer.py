65d0931b6c48c29c533ed6a18ca3343657658efa
Harshal Sheth <hsheth2@gmail.com>
2021-02-04 20:52:47 -0800

    Use better default schema

--- avrogen/core_writer.py
+++ avrogen/core_writer.py
@@ -42,7 +42,7 @@ def convert_default(full_name, idx, do_json=True):
         return (f'_json_converter.from_json_object({full_name}Class.RECORD_SCHEMA.fields[{idx}].default,'
                + f' writers_schema={full_name}Class.RECORD_SCHEMA.fields[{idx}].type)')
     else:
-        return f'SCHEMA.field_map["{idx}"].default'
+        return f'self.RECORD_SCHEMA.field_map["{idx}"].default'
 
 
 def write_defaults(record, writer, my_full_name=None, use_logical_types=False):
