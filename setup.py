8528f4429491eac566b4bf98509926cfa8c3a384
Harshal Sheth <hsheth2@gmail.com>
2021-04-14 12:12:51 -0700

    release 0.4.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setup(
     name='avro-gen3',
-    version='0.3.9',
+    version='0.4.0',
 
     description='Avro record class and specific record reader generator',
     long_description=long_description,
